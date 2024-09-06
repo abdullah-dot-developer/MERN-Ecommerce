0e95c4c3add3712cf01c3796cc7d4344ede313c7
abdullah-dot-programmer <a22795101@gmail.com>
2024-09-06 10:02:41 +0500

    Production URL added

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ import { connectDB } from "./lib/db.js";
 dotenv.config();
 const app = express();
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://mern-ecommerce-ie0r.onrender.com",
     credentials: true
 }))
 const PORT = process.env.PORT || 8000;
